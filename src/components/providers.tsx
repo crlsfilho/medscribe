34ec453dcfc26b5f3c7030034d4a6cfa4d98b3de
Carlos Filho <carlosfilho@Carloss-MacBook-Air.local>
2026-05-09 20:34:08 -0300

    posthog injector

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,14 +3,26 @@
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN, {
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
+    person_profiles: "identified_only",
+  });
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
-      <SessionProvider>
-        {children}
-        <Toaster position="top-right" />
-      </SessionProvider>
-    </ThemeProvider>
+    <PostHogProvider client={posthog}>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+        <SessionProvider>
+          {children}
+          <Toaster position="top-right" />
+        </SessionProvider>
+      </ThemeProvider>
+    </PostHogProvider>
   );
 }
